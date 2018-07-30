43ec8ddad6adefd839b477fdfbd15c57686e7fd8
Victor <victorcapelini@hotmail.com>
2018-07-30 11:49:32 -0300

    Adicionado incremento do contador as imagens

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,5 +2,10 @@ $(".figura").click(function () {
     var url = $(this).find("img").attr("src");
     $("#myModal img").attr("src", url);
     $("#myModal").modal("show");
+    
+    var visualizacoes = $(this).find("p").text().split(" ");
+    visualizacoes[0]++;
+    $(this).find("p").text(visualizacoes.join(" "));
+    console.log(visualizacoes);
 });
 
